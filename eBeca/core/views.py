bcb2f27cb11f0d85eaa6af99d587e75490841aa6
Lucas Garzuze Cordeiro <lucasgarzuze@gmail.com>
2023-07-27 11:26:38 -0300

    Frontpage interage com database
    Agora, a página inicial interage de fato com a base de dados dos produtos existentes. Por enquanto só temos dois tipos de produtos, mas conforme formos expandindo o negócio teremos mais opções.

--- eBeca/core/views.py
+++ eBeca/core/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
 
+from product.models import Product
+
 # Create your views here.
 
 def frontpage(request):
-    return render(request, 'core/frontpage.html')
+    products = Product.objects.all()
+
+    return render(request, 'core/frontpage.html', {'products': products})
+
+def shop(request):
+    products = Product.objects.all()
+
+    return render(request, 'core/shop.html', {'products': products})
