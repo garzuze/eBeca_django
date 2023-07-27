d6bd1c4edc14552fb1946d3132a07517d4fd369a
Lucas Garzuze Cordeiro <lucasgarzuze@gmail.com>
2023-07-27 10:43:05 -0300

    Criando aplicação product
    Essa aplicação contém dois modelos: um de categoria de produtos, outro de produtos em si. Usarei essas databases para registrar os produtos que poderão ser comprados pelos cilentes

--- eBeca/eBeca/settings.py
+++ eBeca/eBeca/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'core.apps.CoreConfig'
+    'core.apps.CoreConfig',
+    'product.apps.ProductConfig'
 ]
 
 MIDDLEWARE = [
