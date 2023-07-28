8894b0887758d787c65423c5b769ec14cf24e8c1
Lucas Garzuze Cordeiro <lucasgarzuze@gmail.com>
2023-07-28 14:42:24 -0300

    Correção de erro no urls.py
    Um erro bobo mas perigoso!! Eu esqueci de colocar a barra (/) ao final URL. Já havia cometido o mesmo erro em outro projeto e me deu uma dor de cabeça para achar.

--- eBeca/eBeca/urls.py
+++ eBeca/eBeca/urls.py
@@ -6,7 +6,7 @@ from product.views import product
 
 urlpatterns = [
     path('', frontpage, name="frontpage"),
-    path('shop', shop, name="shop"),
-    path('product', product, name="product"),
+    path('shop/', shop, name="shop"),
+    path('product/', product, name="product"),
     path('admin/', admin.site.urls),
 ]
