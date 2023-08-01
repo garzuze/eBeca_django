da73f340e439ea02769fc1b95e502fff7d1f0c13
Lucas Garzuze Cordeiro <lucasgarzuze@gmail.com>
2023-08-01 09:39:57 -0300

    Adicionando imagens para os produtos
    Agora é possível adicionar imagens específicas para cada produto. É só colocar a imagem no produto no admin, e o código irá criar uma thumbnail para a imagem, que será utilizada nas páginas de compra.

--- eBeca/product/models.py
+++ eBeca/product/models.py
@@ -1,4 +1,8 @@
 from django.db import models
+from django.core.files import File
+
+from io import BytesIO
+from PIL import Image
 
 # Create your models here.
 
@@ -21,7 +25,9 @@ class Product(models.Model):
     price = models.IntegerField()
     created_at = models.DateTimeField(auto_now_add=True)
     quantity = models.IntegerField()
-
+    image = models.ImageField(upload_to='uploads/', blank=True, null=True)
+    thumbnail = models.ImageField(upload_to='uploads/', blank=True, null=True)
+    
     class Meta:
         # classe para ordernar dos produtos mais recentes para os mais velhos
         ordering = ('-created_at',)
@@ -30,4 +36,30 @@ class Product(models.Model):
         return self.name
     
     def display_price(self):
+        """Mostra o preço em reais do produto (não em centavos)"""
         return int(self.price) / 100
+    
+    # thumbnail é a imagem num formato menor, que é utilizada na hora de listar os produtos
+    def get_thumbnail(self):
+        """Gera a thumbnail para ser utilizada nas páginas de compra"""
+        if self.thumbnail:
+            return self.thumbnail.url
+        else:
+            if self.image:
+                self.thumbnail = self.make_thumbnail(self.image)
+                self.save()
+
+                return self.thumbnail.url
+            else:
+                # retorna uma imagem genérica para caso não exista imagem do produtos
+                return 'https://via.placeholder.com/240x240x.jpg'
+            
+    def make_thumbnail(self, image, size=(300, 300)):
+        """Criar thumbnail a partir da imagem fornecida"""
+        img = Image.open(image).convert('RGB').thumbnail(size)
+        thumb_io = BytesIO()
+        img.save(thumb_io, 'JPEG', quality=85)
+
+        thumbnail = File(thumb_io, name=image.name)
+
+        return thumbnail
