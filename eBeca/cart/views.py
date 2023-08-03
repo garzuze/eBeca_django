f97fe208c1071c85cd67817aa4877412302fc5bd
Lucas Garzuze Cordeiro <lucasgarzuze@gmail.com>
2023-08-03 20:24:40 -0300

    Adicionar ao carrinho com htmx
    Agora adicionei uma funcionalidade simples: ao clicar em adicionar ao carrinho, aparecerá uma bolinha vermelha acima do ícone de sacola de compras indicando quantos itens há no carrinho. Fiz isso utilizando htmx, o que deixou todo o processo muito mais simples!

--- eBeca/cart/views.py
+++ eBeca/cart/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .cart import Cart
+
+def add_to_cart(request, product_id):
+    cart = Cart(request)
+    cart.add(product_id)
+    num_visits = request.session.get('num_visits', 0) + 1
+    request.session['num_visits'] = num_visits
+
+    return render(request, 'cart/menu_cart.html')
+
+def cart(request):
+    return render(request, 'cart/cart.html')
--- eBeca/eBeca/urls.py
+++ eBeca/eBeca/urls.py
@@ -4,6 +4,7 @@ from django.contrib import admin
 from django.contrib.auth import views
 from django.urls import path
 
+from cart.views import add_to_cart, cart
 from core.views import frontpage, shop, user_login, signup
 from product.views import product
 
@@ -14,5 +15,6 @@ urlpatterns = [
     path('login/', views.LoginView.as_view(template_name = 'core/login.html'), name="user_login"),
     path('shop/', shop, name="shop"),
     path('shop/<slug:slug>/', product, name="product"),
+    path('add_to_cart/<int:product_id>/', add_to_cart, name="add_to_cart"),
     path('admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
