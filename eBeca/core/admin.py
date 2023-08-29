9641af69e17afcfcd5f39b5489a331bd25de7739
Lucas Garzuze Cordeiro <lucasgarzuze@gmail.com>
2023-08-29 16:48:40 -0300

    Criando extensão do objeto User para salvar dados
    Estou criando uma extensão do usuário, com novos dados, que será utilizada para adicionar dados como cep, endereço e telefone na página de checkout. Então, a criação de usuário permanece a default oferecida pelo Django, entretanto, agora existe um modelo UserProfile com uma chave um-pra-um que aponta para o usuário e adiciona essas novas informações. Ainda não tenho certeza se essa é a melhor forma de solução para o problema. Talvez eu mude isso depois.

--- eBeca/core/admin.py
+++ eBeca/core/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
 
-# Register your models here.
+from core.models import UserProfile
+from django.contrib.auth.models import User
+from django.contrib.auth.admin import UserAdmin as AuthUserAdmin
+
+# Criando inline para adicionar informações adicionais sobre o usuário na
+# página de usuários do admin
+class UserProfileInline(admin.StackedInline):
+    model = UserProfile
+    can_delete = False
+
+
+class AccountUserAdmin(AuthUserAdmin):
+    # Apenas a view de alterar o usuário deve ter os inlines
+    # Pois o inline causa um erro na add_view, pois o UserProfile só é criado depois do user
+    def add_view(self, *args, **kwargs):
+        self.inlines = []
+        return super(AccountUserAdmin, self).add_view(*args, **kwargs)
+    
+    def change_view(self, *args, **kwargs):
+        self.inlines = [UserProfileInline]
+        return super(AccountUserAdmin, self).change_view(*args, **kwargs)
+
+    inlines = [UserProfileInline]
+
+
+admin.site.unregister(User)
+admin.site.register(User, AccountUserAdmin)
--- eBeca/core/models.py
+++ eBeca/core/models.py
@@ -1,3 +1,21 @@
 from django.db import models
-
+from django.contrib.auth.models import User
+from django.dispatch import receiver
+from django.db.models.signals import post_save
 # Create your models here.
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    cep = models.CharField(max_length=8, null=True, blank=True)
+    address = models.CharField(max_length=150, null=True, blank=True)
+    phone = models.CharField(max_length=11)  
+
+    def __str__(self):
+        return self.user.username
+
+# Capturando um sinal
+# Nós utilizamos dados do usuário recém criados para criar uma extensão do user (profile)
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        UserProfile.objects.create(user=instance)
